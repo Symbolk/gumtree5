b5aad4c5ebdf46ca9274153f047dd8ae1ee52427
Symbolk <symbolk@163.com>
2019-07-30 22:18:35 +0800

    [feature] Add simple data collector to collect experiment data.

---
+++ client/src/main/java/com/github/gumtreediff/client/DataCollector.java
@@ -0,0 +1,32 @@
+package com.github.gumtreediff.client;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class DataCollector {
+    public static void main(String[] args) {
+        String REPO_NAME = "guava";
+        String REPO_DIR = "D:\\github\\repos\\" + REPO_NAME;
+        String DATA_DIR = "D:\\commit_data";
+        String commitID = "dcf63a6c97dfde";
+
+        ArrayList<FilePair> filePairs = Utils.getChangedFilesAtCommit(REPO_DIR, commitID);
+        // write old/new content to disk
+        for (FilePair filePair : filePairs) {
+            // currently only collect MODIFIED Java files
+            if (filePair.getOldPath().endsWith(".java") && filePair.getChangeType().equals(ChangeType.MODIFIED)) {
+                String dir = DATA_DIR + File.separator + REPO_NAME + File.separator + commitID + File.separator;
+                String aPath = dir + "a" + File.separator + filePair.getOldPath();
+                String bPath = dir + "b" + File.separator + filePair.getNewPath();
+                boolean aOk = Utils.writeContentToPath(aPath, filePair.getOldContent());
+                boolean bOk = Utils.writeContentToPath(bPath, filePair.getNewContent());
+                if (!(aOk && bOk)) {
+                    System.out.println("Error with: " + filePair.getOldPath());
+                } else {
+                    System.out.println(aPath);
+                    System.out.println(bPath);
+                }
+            }
+        }
+    }
+}
--- client/src/main/java/com/github/gumtreediff/client/Utils.java
+++ client/src/main/java/com/github/gumtreediff/client/Utils.java
@@ -1,9 +1,6 @@
 package com.github.gumtreediff.client;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -91,6 +88,56 @@ public class Utils {
         return filePairList;
     }
 
+    /**
+     * Parse the output of git diff SHA and return changed file pairs
+     *
+     * @return
+     */
+    public static ArrayList<FilePair> getChangedFilesAtCommit(String repoDir, String commitID) {
+        String output =
+                Utils.runSystemCommand(
+                        repoDir,
+                        "git",
+                        "diff",
+                        "--name-status", commitID, commitID + "~");
+        ArrayList<FilePair> filePairList = new ArrayList<>();
+        String lines[] = output.split("\\r?\\n");
+        for (int i = 0; i < lines.length; i++) {
+            String temp[] = lines[i].trim().split("\\s+");
+            String symbol = temp[0];
+            String relativePath = temp[1];
+//            String absolutePath = repoDir + File.separator + relativePath;
+            ChangeType changeType = getTypeFromSymbol(symbol);
+            FilePair filePair = null;
+            switch (changeType) {
+                case MODIFIED:
+                    filePair = new FilePair(changeType, relativePath, relativePath, getContentAtCommit(repoDir, relativePath, commitID + "~"), getContentAtCommit(repoDir, relativePath, commitID));
+                    break;
+                case ADDED:
+                case UNTRACKED:
+                    filePair = new FilePair(changeType, "", relativePath, "", getContentAtCommit(repoDir, relativePath, commitID));
+                    break;
+                case DELETED:
+                    filePair = new FilePair(changeType, relativePath, "", getContentAtCommit(repoDir, relativePath, commitID + "~"), "");
+                    break;
+                case RENAMED:
+                case COPIED:
+                    if (temp.length == 4) {
+                        String oldPath = temp[1];
+                        String newPath = temp[3];
+                        filePair = new FilePair(changeType, oldPath, newPath, getContentAtCommit(repoDir, oldPath, commitID + "~"), getContentAtCommit(repoDir, newPath, commitID));
+                    }
+                    break;
+                default:
+                    break;
+            }
+            if (filePair != null) {
+                filePairList.add(filePair);
+            }
+        }
+        return filePairList;
+    }
+
     private static ChangeType getTypeFromSymbol(String symbol) {
         for (ChangeType type : ChangeType.values()) {
             if (symbol.equals(type.symbol)) {
@@ -119,6 +166,24 @@ public class Utils {
     }
 
     /**
+     * Get the file content at commit
+     *
+     * @param relativePath
+     * @return
+     */
+    private static String getContentAtCommit(String repoDir, String relativePath, String commitID) {
+        String output = runSystemCommand(repoDir,
+                "git",
+                "show",
+                commitID + ":" + relativePath);
+        if (output != null) {
+            return output;
+        } else {
+            return "";
+        }
+    }
+
+    /**
      * Read the content of a given file.
      *
      * @param path to be read
@@ -139,6 +204,63 @@ public class Utils {
             System.err.println(path + " does not exist!");
         }
         return content;
+    }
+
+    /**
+     * Writes the given content in the file of the given file path.
+     *
+     * @param filePath
+     * @param content
+     * @return boolean indicating the success of the write operation.
+     */
+    public static boolean writeContentToPath(String filePath, String content) {
+        if (!content.isEmpty()) {
+            try {
+                File file = new File(filePath);
+                if (!file.exists()) {
+                    file.getParentFile().mkdirs();
+                    file.createNewFile();
+                }
+                BufferedWriter writer = Files.newBufferedWriter(Paths.get(filePath));
+                writer.write(content);
+                writer.flush();
+                writer.close();
+            } catch (NullPointerException ne) {
+                ne.printStackTrace();
+                // empty, necessary for integration with git version control system
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Delete all files and subfolders to clear the directory
+     *
+     * @param dir absolute path
+     * @return
+     */
+    public static boolean clearDir(String dir) {
+        File file = new File(dir);
+        if (!file.exists()) {
+            return false;
+        }
+
+        String[] content = file.list();
+        for (String name : content) {
+            File temp = new File(dir, name);
+            if (temp.isDirectory()) {
+                clearDir(temp.getAbsolutePath());
+                temp.delete();
+            } else {
+                if (!temp.delete()) {
+                    System.err.println("Failed to delete the directory: " + name);
+                }
+            }
+        }
+        return true;
     }
 }
 
